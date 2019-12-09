9c0731407d34ccf76c8d98aaa3b3fec0b2831c6a
Eli Jones <eli.jones@gmail.com>
2019-12-09 16:54:26 -0500

    app.py: minor note - only use 1 worker with gunicorn and gevent.

--- app.py
+++ app.py
@@ -57,6 +57,6 @@ if __name__ == '__main__':
     """
       TODO: replace threaded with regular gunicorn with gevent.
       
-      gunicorn --worker-class=gevent --workers=4 --bind=0.0.0.0:3000 --log-level=debug app:app
+      gunicorn --worker-class=gevent --workers=1 --bind=0.0.0.0:3000 --log-level=debug app:app
     """
     app.run(host='0.0.0.0', port=3000, debug=True)
